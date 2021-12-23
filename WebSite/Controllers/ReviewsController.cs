bc70a6ff9cde473af861dfa6a44f4bf0ddacc31a
Emirhan Şensoy <emirhn.sensoy@gmail.com>
2021-12-23 17:04:50 +0300

    API service for reviews.

---
+++ WebSite/Controllers/ReviewsController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebSite.Data;
+using WebSite.Models;
+
+namespace WebSite.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReviewsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ReviewsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Reviews
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Review>>> GetReviews()
+        {
+            return await _context.Reviews.ToListAsync();
+        }
+
+        // GET: api/Reviews/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Review>> GetReview(int id)
+        {
+            var review = await _context.Reviews.FindAsync(id);
+
+            if (review == null)
+            {
+                return NotFound();
+            }
+
+            return review;
+        }
+    }
+}
